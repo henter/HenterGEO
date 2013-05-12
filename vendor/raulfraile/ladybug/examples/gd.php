14f35111362d26ef12d706a4167a5cac12f58d46
henter <henter@henter.me>
2013-05-12 16:12:11 +0800

    del vender

--- vendor/raulfraile/ladybug/examples/gd.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__.'/../vendor/autoload.php';
-Ladybug\Loader::loadHelpers();
-
-$img = imagecreatefrompng(__DIR__ . '/images/ladybug.png');
-
-ladybug_dump($img);
--- vendor/raulfraile/ladybug/examples/resources.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__.'/../vendor/autoload.php';
-Ladybug\Loader::loadHelpers();
-
-$file = fopen(__DIR__ . '/../LICENSE', 'r');
-
-ladybug_dump($file);
